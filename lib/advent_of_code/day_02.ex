e240bb96147198768818eb53dc4eeb0003f0da99
glomph <glompho@gmail.com>
2023-12-03 00:56:18 +0000

    Day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,58 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  @max_key %{"red" => 12, "green" => 13, "blue" => 14}
+
+  def find_highest([s]) do
+    s
+    |> String.replace(~r/[^0-9redgreenblue\s]/, "")
+    |> String.split(" ", trim: true)
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [a, b] -> {String.to_integer(a), b} end)
+    |> Enum.group_by(&elem(&1, 1))
+    |> Enum.map(fn {colour, values} ->
+      {colour, Enum.reduce(Enum.map(values, &elem(&1, 0)), &max/2)}
+    end)
   end
 
-  def part2(_args) do
+  def filter_games(s) do
+    s
+    |> Enum.map(fn {colour, value} ->
+      value <= @max_key[colour]
+    end)
+    |> Enum.all?()
+  end
+
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn s ->
+      s
+      |> String.split([":"])
+      |> tl
+      |> find_highest()
+      |> filter_games()
+    end)
+    |> Enum.with_index()
+    |> Enum.map(fn {value, index} ->
+      if value do
+        index + 1
+      else
+        0
+      end
+    end)
+    |> Enum.reduce(&+/2)
+  end
+
+  def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn s ->
+      s
+      |> String.split([":"])
+      |> tl
+      |> find_highest()
+      |> Enum.map(fn {_c, number} -> number end)
+    end)
+    |> Enum.map(fn s -> Enum.reduce(s, &*/2) end)
+    |> Enum.reduce(&+/2)
   end
 end
