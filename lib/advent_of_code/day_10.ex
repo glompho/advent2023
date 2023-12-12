0c8adaad45d70c8c8751f395e9554139624d49da
glomph <glompho@gmail.com>
2023-12-12 22:56:44 +0000

    Done part one but more to do for part 2

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,112 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_args) do
+  @valid_map %{
+    {-1, 0} => ["|", "7", "F"],
+    {1, 0} => ["|", "L", "J"],
+    {0, -1} => ["-", "L", "F"],
+    {0, 1} => ["-", "7", "J"]
+  }
+  @move_map %{
+    "-" => [{0, 1}, {0, -1}],
+    "|" => [{1, 0}, {-1, 0}],
+    "7" => [{1, 0}, {0, -1}],
+    "F" => [{1, 0}, {0, 1}],
+    "J" => [{-1, 0}, {0, -1}],
+    "L" => [{-1, 0}, {0, 1}]
+  }
+
+  def parse(args) do
+    args
+    |> String.split(["\n", "\r"], trim: true)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, line_index} ->
+      line
+      |> String.split("", trim: true)
+      |> Enum.with_index()
+      |> Enum.map(fn {char, char_index} ->
+        {{line_index, char_index}, char}
+      end)
+    end)
+    |> Map.new()
   end
 
-  def part2(_args) do
+  def find_valid_next({x, y}, map) do
+    for {i, j} <- Map.keys(@valid_map) do
+      char = Map.get(map, {x + i, j + y}, nil)
+
+      if char in @valid_map[{i, j}] do
+        {x + i, j + y}
+      else
+        nil
+      end
+    end
+    |> Enum.filter(&(&1 != nil))
+  end
+
+  def follow_path({x, y}, map, route) do
+    # IO.inspect({{x, y}, route})
+    char = Map.get(map, {x, y})
+
+    if char == "S" do
+      route
+    else
+      next_loc =
+        Map.get(@move_map, char)
+        |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
+        |> Enum.filter(fn new -> new != hd(route) end)
+        |> hd
+
+      # IO.inspect({next_loc, [{x, y}] ++ route})
+      follow_path(next_loc, map, [{x, y}] ++ route)
+    end
+  end
+
+  def find_path(map) do
+    {start_x, start_y} =
+      map
+      |> Enum.find(fn {_key, val} -> val == "S" end)
+      |> elem(0)
+
+    {next_x, next_y} = find_valid_next({start_x, start_y}, map) |> hd
+
+    follow_path({next_x, next_y}, map, [{start_x, start_y}])
+  end
+
+  def part1(args) do
+    map =
+      parse(args)
+
+    route_len =
+      find_path(map)
+      |> Enum.count()
+
+    Float.floor(route_len / 2)
+  end
+
+  def shoe_lace(path) do
+    length = Enum.count(path)
+
+    s =
+      path
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [{xi, yi}, {xj, yj}] ->
+        (yi + yj) * (xi - xj)
+      end)
+      |> Enum.sum()
+
+    area = 0.5 * s
+  end
+
+  def flood_fill(map, path) do
+    # is this the way?
+    nil
+  end
+
+  def part2(args) do
+    map = parse(args)
+
+    path = find_path(map)
+    IO.inspect(Enum.count(path))
+    # this doesn't seem to get anywhere near the truth.
+    area = shoe_lace(path)
   end
 end
--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,19 +3,58 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF"
     result = part1(input)
 
-    assert result
+    assert result == 4
   end
 
-  @tag :skip
+  test "part1_test2" do
+    input =
+      "7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ"
+
+    result = part1(input)
+
+    assert result == 8
+  end
+
   test "part2" do
-    input = nil
+    input = "...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."
     result = part2(input)
 
-    assert result
+    assert result == 4
+  end
+
+  test "part2_b" do
+    input = ".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."
+    result = part2(input)
+
+    assert result == 8
   end
 end
